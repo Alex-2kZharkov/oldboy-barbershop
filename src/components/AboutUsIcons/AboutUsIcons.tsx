878c955c9cdd961eecbe873216764994458f51de
bubumariia <mariiakudaiberdieva@gmail.com>
2023-09-25 15:19:58 +0300

    Update AboutUsIcons.tsx
    updated img to Image from next/Image

--- src/components/AboutUsIcons/AboutUsIcons.tsx
+++ src/components/AboutUsIcons/AboutUsIcons.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
+import Image from 'next/image';
 
 type AboutUsIconProps = {
   src: string;
   alt?: string;
   text: string;
+  width: number;
+  height: number;
 };
 
-export const AboutUsIcon: React.FC<AboutUsIconProps> = ({ src, alt = 'About Us', text }) => {
+export const AboutUsIcon: React.FC<AboutUsIconProps> = ({ src, alt = 'About Us', text, width, height }) => {
   return (
     <div className="flex-col items-center justify-center py-5">
-      <img src={src} alt={alt} className="w-20 mx-auto mb-5" />
+      <div className="relative w-20 h-auto mx-auto mb-5">
+        <Image src={src} alt={alt} layout="fill" objectFit="contain" />
+      </div>
       <p
         className="text-center  
         text-orange-bg 
